773888f32333d3e039e71e7655a2c4fcdc0576ae
Odd Magnus Meyer <s236639@stud.hioa.no>
2017-01-30 09:53:25 +0100

    Installert ZXing og SQLite.net i [portable] prosjektet, påbegynt databasestruktur

---
+++ Databar skanner/Databar skanner/Databar_skanner/Database/Rabatt.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Databar_skanner.Database
+{
+    class Rabatt
+    {
+        [PrimaryKey]
+        public int Kategori { get; set; }
+        public double ToDagerRabatt { get; set; }
+        public double EnDagRabatt { get; set; }
+    }
+}
