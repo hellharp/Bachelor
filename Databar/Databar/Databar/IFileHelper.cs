ef448ce7ddc16eb45b28b72073b12f11f160af55
hellharp <s236639@stud.hioa.no>
2017-02-01 12:25:29 +0100

    Påbegynt testoppretting av db, lagt til hjelpefil IFileHelper

---
+++ Databar/Databar/Databar/IFileHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Databar
+{
+    // Fra https://developer.xamarin.com/guides/xamarin-forms/working-with/databases/
+    public interface IFileHelper
+    {
+        string GetLocalFilePath(string filename);
+    }
+}
---
+++ Databar/Databar/Databar/dbTestOpprett.cs
@@ -0,0 +1,19 @@
+﻿using SQLite.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Databar
+{
+    class dbTestOpprett
+    {
+        private string DbName = "Test_DB.db";
+        string path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+        SQLiteConnection dbConn = new SQLiteConnection(System.IO.Path.Combine(path, DbName));
+
+        dbConn
+
+    }
+}
