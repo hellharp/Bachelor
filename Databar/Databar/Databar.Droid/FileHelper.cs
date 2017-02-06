58955399cbe5e7153d51a6823f25341d772b324f
hellharp <s236639@stud.hioa.no>
2017-02-06 11:03:43 +0100

    Fikset på FileHelper i .Droid

--- Databar/Databar/Databar.Droid/FileHelper.cs
+++ Databar/Databar/Databar.Droid/FileHelper.cs
@@ -2,18 +2,17 @@ using System;
 using System.IO;
 using Xamarin.Forms;
 using Databar.Droid;
+using Databar;
 
-
-
-//[assembly: Dependency(typeof(FileHelper))]
-//namespace Databar.Droid
-//{
-//    public class FileHelper : IFileHelper
-//    {
-//        public string GetLocalFilePath(string filename)
-//        {
-//            string path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
-//            return Path.Combine(path, filename);
-//        }
-//    }
-//}
+[assembly: Dependency(typeof(FileHelper))]
+namespace Databar.Droid
+{
+    public class FileHelper : IFileHelper
+    {
+        public string GetLocalFilePath(string filename)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            return Path.Combine(path, filename);
+        }
+    }
+}
--- Databar/Databar/Databar.Droid/obj/Debug/android/databar/droid/R.java
+++ Databar/Databar/Databar.Droid/obj/Debug/android/databar/droid/R.java
@@ -2793,6 +2793,8 @@ containing a value of this type.
         public static final int toolbar=0x7f030038;
     }
     public static final class string {
+        public static final int ApplicationName=0x7f080027;
+        public static final int Hello=0x7f080026;
         public static final int abc_action_bar_home_description=0x7f08000f;
         public static final int abc_action_bar_home_description_format=0x7f080010;
         public static final int abc_action_bar_home_subtitle_description_format=0x7f080011;
