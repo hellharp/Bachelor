f3ece7fc1ac691aeae98c28186d1a9ad4f718eca
Vidar Vasrud <s159718@stud.hioa.no>
2017-02-06 10:15:22 +0100

    La til linje 2 - 4 og fjernet unødvendige "using" i FileHelper.cs.

--- Databar/Databar/Databar.Droid/FileHelper.cs
+++ Databar/Databar/Databar.Droid/FileHelper.cs
@@ -1,14 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
+using Xamarin.Forms;
+using Databar.Droid;
 
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 
 
 [assembly: Dependency(typeof(FileHelper))]
