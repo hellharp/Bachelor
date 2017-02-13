de20ea063608de54939b856fb74651b5a095c2b5
hellharp <s236639@stud.hioa.no>
2017-02-13 12:09:29 +0100

    Fjernet CategoryRebate, lagt til rabatter og serienr i Product

--- Databar/Databar/Databar/Models/Product.cs
+++ Databar/Databar/Databar/Models/Product.cs
@@ -17,12 +17,28 @@ namespace Databar_skanner.Models
         [MaxLength(14)]
         public long GTIN { get; set; }
 
-        [ForeignKey(typeof(CategoryRebate))]
-        public string Category { get; set; }
-
+        // Produktbeskrivelse
         public string ProductName { get; set; }
 
         // Utløpsdato dekker best før/siste forbruksdato
         public DateTime BestBeforeDate { get; set; }
+
+        // Serienummer
+        public string SerialNumber { get; set; }
+
+        // Fire dager før utløpsdato
+        public double FourDaysRebate { get; set; }
+
+        // Tre dager før utløpsdato
+        public double ThreeDaysRebate { get; set; }
+
+        // To dager før utløpsdato
+        public double TwoDaysRebate { get; set; }
+
+        // En dag før utløpsdato
+        public double OneDayRebate { get; set; }
+
+        // På utløpsdatoen
+        public double LastDayRebate { get; set; }
     }
 }
