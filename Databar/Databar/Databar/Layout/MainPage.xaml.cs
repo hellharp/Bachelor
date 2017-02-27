9c831950d611e478e8c0d7c1b54adb1155ff851b
Eirik Stensbøl <s929596@stud.hioa.no>
2017-02-27 11:54:56 +0100

    Revert "test stage"
    This reverts commit 461eb0b39db890af36fcabd2bccff7bd51fce208.

--- Databar/Databar/Databar/Layout/MainPage.xaml.cs
+++ Databar/Databar/Databar/Layout/MainPage.xaml.cs
@@ -15,34 +15,34 @@ namespace Databar.Layout
             InitializeComponent();
         }
 
-        async void ToZXing(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new BasisUI());
+		async void ToZXing(object sender, EventArgs e)
+		{
+			await Navigation.PushAsync(new BasisUI());
 
-            //361923d... Commit av div endringer + paypage og PayConfirmationPage
-        }
+		//361923d... Commit av div endringer + paypage og PayConfirmationPage
+		}
 
-        async void ToEditProductPage(object sender, EventArgs e)
-        {
+		async void ToEditProductPage(object sender, EventArgs e)
+		{
 
-            await Navigation.PushAsync(new EditProductPage());
-        }
+			await Navigation.PushAsync(new EditProductPage());
+		}
 
-        async void ToAdminLogin(object sender, EventArgs e)
-        {
-            await Navigation.PushModalAsync(new LoginModal());
-            //361923d... Commit av div endringer + paypage og PayConfirmationPage
-        }
+		async void ToAdminLogin(object sender, EventArgs e)
+		{
+			await Navigation.PushModalAsync(new LoginModal());
+		//361923d... Commit av div endringer + paypage og PayConfirmationPage
+		}
 
-        async void ToPayPage(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync((new PayPage()));
-        }
+		async void ToPayPage(object sender, EventArgs e)
+		{
+			await Navigation.PushAsync((new PayPage()));	
+		}
 
-        async void ToPayConfirmationPage(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new PayConfirmationPage());
-        }
+		async void ToPayConfirmationPage(object sender, EventArgs e)
+		{
+			await Navigation.PushAsync(new PayConfirmationPage());
+		}
 
-    }
+	}
 }
