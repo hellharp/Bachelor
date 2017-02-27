2c5bf8847d1d38de428ff07852fca9826d390aab
hellharp <s236639@stud.hioa.no>
2017-02-27 12:41:43 +0100

    Lagt til lukke-melding i MainPage ved back-button-press

--- Databar/Databar/Databar/Layout/MainPage.xaml.cs
+++ Databar/Databar/Databar/Layout/MainPage.xaml.cs
@@ -45,5 +45,27 @@ namespace Databar.Layout
 			await Navigation.PushAsync(new PayConfirmationPage());
 		}
 
+
+		//Handle device hardware back button to prevent accidental closing of app
+		private bool _canClose = true;
+		protected override bool OnBackButtonPressed()
+		{
+			if (_canClose)
+			{
+				ShowExitDialog();
+			}
+			return _canClose;
+		}
+		// Exit dialog. NOTE: User has to press back-button again to actually close app
+		private async void ShowExitDialog()
+		{
+			var answer = await DisplayAlert("Advarsel!", "Vil du lukke appen?", "Ja", "Nei");
+			if (answer)
+			{
+				_canClose = false;
+				OnBackButtonPressed();
+			}
+		}
+
 	}
 }
