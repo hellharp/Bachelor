2759f463f4dc098b50a16d7c7db9125ea6ebb618
wiswas <wiswas@outlook.com>
2017-03-31 14:33:09 +0200

    Changed to menubutton and helpbutton in PayPage. Removed GS1 logo on PayConfirmationPage. Made a popupwindow from the helpbutton in PayPage. Both pages works horizontally.

--- Databar/Databar/Databar/Views/PayPage.xaml.cs
+++ Databar/Databar/Databar/Views/PayPage.xaml.cs
@@ -24,5 +24,19 @@ namespace Databar.Views
 		{
 			await Navigation.PushAsync(new ShoppingCart());
 		}
-    }
+
+		async void ToMainPage(object sender, EventArgs e)
+		{
+			await Navigation.PushAsync(new MainPage());
+		}
+
+		async void ToPopUp(object sender, EventArgs e)
+		{
+			await DisplayAlert("Informasjon", "Du velger betalingsmetode avhengig av hvilken betalingsapp du ønsker å betale med. " +
+			                   "Ved valgt så betales varene i handlekurven.\n\n" +
+			                   "Handlekurvknappen til venstre i bunnen sender deg tilbake handlekurven uten å ha betalt.\n\n" +
+			                   "Menyknappen i midten i bunnen sender deg tilbake til hovedmenyen uten å ha betalt.", "Lukk");
+		}
+
+	}
 }
