56b5987a0fce38cbbf56514dcb54a42f3c37459a
s929596 <s929596@stud.hioa.no>
2017-05-11 23:09:43 +0200

    Update to information page

--- Databar/Databar/Databar/Models/HelpItem.cs
+++ Databar/Databar/Databar/Models/HelpItem.cs
@@ -8,19 +8,22 @@ using Xamarin;
 
 namespace Databar.Models
 {
-    /// <summary>
-    /// Model for each element in a view that might need explanation
-    /// </summary>
-    public class HelpItem
-    {
-        /// <summary>
-        /// String which points to which element that needs explanation if there is no image
-        /// </summary>
-        public string ItemText { get; set; }
-        
-        /// <summary>
-        /// String that explains the chosen element
-        /// </summary>
-        public string Explanation { get; set; }
-    }
+	/// <summary>
+	/// Model for each element in a view that might need explanation
+	/// </summary>
+	public class HelpItem
+	{
+		public string _TextAnd;
+		/// <summary>
+		/// String which points to which element that needs explanation if there is no image
+		/// </summary>
+		public string ItemText { get; set; }
+
+		/// <summary>
+		/// String that explains the chosen element
+		/// </summary>
+		public string Explanation { get; set; }
+
+		public string TextAnd { get { return ItemText + ":\n" + Explanation + "\n"; } set { _TextAnd = value; } }
+	}
 }
